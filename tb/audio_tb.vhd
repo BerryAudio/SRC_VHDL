3a59c5092a816bc81d3d0eff9107a2af4e9b3441
Charlie <charlie@bluecouch.com.au>
2015-03-20 11:33:32 +1000

    Updated sig_gen_pkg to accomodate the generation of multiple sinusoids

--- tb/audio_tb.vhd
+++ tb/audio_tb.vhd
@@ -40,10 +40,7 @@ ENTITY audio_tb IS
 END audio_tb;
  
 ARCHITECTURE behavior OF audio_tb IS 
-	constant s_width	: natural := 35;
 	constant s_rate	: real 	 := 96.0;
-	constant s_freq	: real 	 := 1.0;
- 
  
    --Inputs
    signal clk_24 : std_logic := '0';
@@ -235,7 +232,7 @@ BEGIN
    stim_proc: process
    begin		
       -- hold reset state for 100 ns.
-		set_sig( s_freq, s_rate, s_width );
+		set_rate( s_rate );
 		wait until ctrl_rdy = '1';
 		
 		if		s_rate =  44.1 then spi_write( "10000000", spi_data, spi_en );
--- tb/i2s_util_tb.vhd
+++ tb/i2s_util_tb.vhd
@@ -18,21 +18,39 @@ ENTITY i2s_util_tb IS
 END i2s_util_tb;
  
 ARCHITECTURE behavior OF i2s_util_tb IS 
+	constant freq0			: real :=  1.0; 
+	constant freq1			: real := 20.0;
+
+	shared variable sig0	: SIG_TYPE := sig_type_init;
+	shared variable sig1	: SIG_TYPE := sig_type_init;
+
 	signal data		: signed( 23 downto 0 ) := ( others => '0' );
 	signal ws_buf	: std_logic_vector( 1 downto 0 ) := "00";
 	signal wsp	 	: std_logic := '0';
 BEGIN
 
+	process
+	begin
+		sig0.freq := freq0;
+		sig1.freq := freq1;
+		wait;
+	end process;
+
 	wsp <= ws_buf( 0 ) xor ws_buf( 1 );
 	i2s_data <= data( 23 );
 
 	i2s_process : process( i2s_bclk )
+		variable sample0	: signed( 22 downto 0 ) := ( others => '0' );
+		variable sample1	: signed( 22 downto 0 ) := ( others => '0' );
 		variable sample	: signed( 23 downto 0 ) := ( others => '0' );
 	begin
 		if falling_edge( i2s_bclk ) then
 			if wsp = '1' then
+			
 				if ws_buf( 0 ) = '0' then
-					sample := fetch_sample( 34 downto 11 );
+					fetch_sample( sig0 ); sample0 := sig0.sig( 34 downto 12 );
+					fetch_sample( sig1 ); sample1 := sig1.sig( 34 downto 12 );
+					sample := RESIZE( sample0, 24 ) + RESIZE( sample1, 24 );
 					data <= sample;
 				else
 					data <= RESIZE( sample( 23 downto 20 ), 24 );
