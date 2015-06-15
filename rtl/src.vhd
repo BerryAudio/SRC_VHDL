a11439d5bb97bbd5267121ca7469ae0ef27c1096
BerryAudio <charlie@bluecouch.com.au>
2015-06-16 09:09:35 +1000

    Dither update
    changed number of bits in dither, and consequently the number of bits in
    the n shaping filter delay line

--- rtl/src.vhd
+++ rtl/src.vhd
@@ -16,8 +16,8 @@ package src is
 	constant RING_BUF_PTR_OFFSET	: integer := 16;
 	constant REG_AVE_WIDTH			: integer range 4 to 6 := 6;
 	
-	constant NOISE_LFSR_WIDTH		: integer := 14;
-	constant NOISE_FILT_WIDTH		: integer := 20;
+	constant NOISE_LFSR_WIDTH		: integer := 12;
+	constant NOISE_FILT_WIDTH		: integer := 18;
 
 	--******************************************************************
 	-- types
